de72aacfa9fb41299808d8bdfb88f92799a20620
BlazeS8123 <167114141+BlazeS8123@users.noreply.github.com>
2025-02-05 13:21:28 +0530

    Add files via upload

---
+++ c++/DoW.cxx
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int s=7;
+  do(s<16){
+    cout<<"s="<<s<<endl;
+    a++;
+  }while(s<16);
+  return 0;
+}
---
+++ c++/While.cxx
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int g=6;
+  while(g<10){
+    cout<<"g="<<g<<endl;
+    g++;
+  }
+  return 0;
+}
