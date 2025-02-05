7431c8b3b705bdc26e22959fccea284c630f7a3d
BlazeS8123 <suhail8s123@gmail.com>
2025-02-05 18:57:42 +0530

    oo

---
+++ c++/ascii.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+	char ch;
+	int no;
+	cout<<"CHARACTER: ";
+	cin>>ch;
+	no=ch;
+	cout<<"ASCII VALUE OF "<<ch<<" = " << no<<endl;
+	return 0;
+}
